bc0f40e02f73b473127c742a1258fb747692b9b3
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2024-08-10 23:35:28 +0300

    add swagger

---
+++ src/bootstrap/addSwagger.ts
@@ -0,0 +1,15 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function addSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Remusic API')
+    .setDescription('Remusic API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { addSwagger } from './bootstrap/addSwagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    addSwagger(app);
+  }
+
   await app.listen(3001);
 
-  if (module.hot) {
+  if (process.env.NODE_ENV !== 'production' && module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
+    addSwagger(app);
   }
 }
 bootstrap();
