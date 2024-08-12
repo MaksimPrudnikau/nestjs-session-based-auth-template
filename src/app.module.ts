25aa7f04cd31798555825c56c8625c5934bdff55
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2024-08-12 10:15:50 +0300

    init auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './bootstrap/prisma.module';
 import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from 'nestjs-zod';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, UsersModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [
     AppService,
